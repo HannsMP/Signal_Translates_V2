6387a4b7a2bac018174b03528dae7df8b3d34dda
= <=>
2025-06-27 17:03:58 -0500

    0.0.9

--- update.bat
+++ update.bat
@@ -1,10 +1,5 @@
 @echo off
 cd /d "%~dp0"
-echo Este script hará un reset --hard del repositorio.
-set /p confirm="¿Deseas continuar? (s/n): "
-if /i "%confirm%"=="s" (
-    echo Haciendo reset hard del repositorio...
-    git fetch origin && git reset --hard origin/main
-) else (
-    echo Operación cancelada.
-)
+echo Haciendo reset hard del repositorio...
+git fetch origin && git reset --hard origin/main
+echo ✅ Repositorio actualizado correctamente.
